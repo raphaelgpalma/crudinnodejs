94a553043e3b1befd380afcbd5ad398752bfbd9f
Raphael Palma <117413668+raphaelgpalma@users.noreply.github.com>
2024-08-16 21:26:49 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize')
+const config = require('../models/database')
+
+const Endereco = require('../models/Endereco')
+
+const connection = new Sequelize(config)
+
+Endereco.init(connection)
+Endereco.associate(connection.models)
+module.exports = connection
